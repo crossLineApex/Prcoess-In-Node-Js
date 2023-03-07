6d93e280308409aa3eaee1c705369a507331ce50
crossLineApex <56408469+crossLineApex@users.noreply.github.com>
2023-03-07 12:20:55 +0530

    Adding SIGINT and SIGTERM Signals

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ console.log("End");
 
 // Process simply is the code which runs in the OS
 // Process in Node js is the program in Node js which runs in the OS
-// Process is the instance of instance of Events in Node js so you can listen to all the events (even error ones) [-- PART 2]
+// Process is the instance of Events in Node js so you can listen to all the events (even error ones) [-- PART 2]
 // Process is a global object
-// process.exit is run by Event Loop when the call stack is empty and even all the event loop queue, but there's a catch
+// process.exit is run by Event Loop when the call stack is empty and even all the event loop queue is empty, but there's a catch
 // process.exit can also be called on its own when there's an error event like uncaughtException 
 // exit returns with an exit code which tells the status of the how the process ended e.g. 0 means a success while 1 means a fatal error, there are many
 // you can change the exit codes
-// while listening to process.exit you can't run any async function as exit is synchronous [--PART 2]
+// while listening to process.exit you can't run any async function, as exit is synchronous [--PART 2]
 
 // ------------------------------------ PART 2 -----------------------------------
 
@@ -154,7 +154,7 @@ console.log("End");
 
 // Now you have it
 // but again there's a catch
-// you can't listen to beforeExit if you call process.exit() is called explicitly or uncaught error event is triggered
+// you can't listen to beforeExit if process.exit() is called explicitly or uncaught error event is triggered
 
 console.log("start");
 
@@ -251,8 +251,8 @@ console.log("End");
 // exit called with code:  0
 
 // uncaughtException can run async function as well
-// there is one issue in the above code, the exit code which we are recieveing is 0, by default, but since we are listening to an error event
-// we should send 1 as exitcode
+// there is one issue in the above code, the exit code which we are recieveing is 0 meaaning success, by default, but since we are listening to an error event
+// we should send 1 as exitcode meaning fatal error
 
 console.log("start");
 
@@ -287,14 +287,98 @@ console.log("End");
 
 
 
+// ------------------------------------ PART 5 -----------------------------------
+
+// Let's talk about Signals
+// Signals are recieved by Node js as event
+// Two signals which you should know about is SIGINT and SIGTERM
+
+// let's dive into SIGINT
+
+console.log("start");
+console.log(process.pid); // the id of the current process
+
+setTimeout(() => {
+    console.log("after 10 sec");
+}, 10000);
+
+process.on("SIGINT",() => {
+    console.log("SIGINT recieved");
+})
+
+console.log("End");
+
+// Output:
+// start
+// 1671
+// End
+// ^CSIGINT recieved
+// after 10 sec
+
+// SIGINT is emitted when you end the server, here pressing ctrl+c in the terminal to exit the server, but it will wait for all async operation to complete that are in event loop
+// and will then end the process
+// you can also run async operation inside SIGINT listener, for e.g. you can log the error to another server and exit
 
 
 
 
+console.log("start");
+console.log(process.pid);
+
+setTimeout(() => {
+    console.log("after 10 sec");
+}, 10000);
+
+console.log("End");
+
+// Output:
+// start
+// 1922
+// End
+// Terminated: 15
+
+// SIGTERM is another event which is emitted when you kill the process in the terminal by saying, kill <process.pid>
+// we are not listening to SIGTERM event above and upon killing the process the entire Node Js process exits
+// Here's a very important point from Node JS official DOCS
+// 'SIGTERM' and 'SIGINT' have default handlers on non-Windows platforms that reset the terminal mode before exiting with code 
+// 128 + signal number. If one of these signals has a listener installed, its default behavior will be removed (Node.js will no longer exit).
+// This means that once you have a listener in your process for SIGTERM it won't terminate the process abruptly
+// but will wait for call stack to be empty and will then exit
+// You can also have have async opeartion inside SIGTERM listener
 
 
+console.log("start");
+console.log(process.pid);
 
+setTimeout(() => {
+    console.log("after 10 sec");
+}, 10000);
 
+process.on("SIGTERM",() => {
+    console.log("SIGTERM recieved");
+    setTimeout(() => {
+        console.log("after 20 sec");
+    }, 20000);
+})
+
+console.log("End");
+
+// Output:
+// start
+// 1985
+// End
+// SIGTERM recieved
+// after 10 sec
+// after 20 sec
+
+// on having a SIGTERM listener now we have altered it's default behaviour
+
+// So if you have not provided any listener to SIGINT or SIGTERM it will
+// 1. end process
+// 2. will not care for event loop, even if any task is remaining
+// 3. therefore will not call exit, even if you have an exit listener
+
+// It's a good practise to have listener for SIGINT and SIGTERM
 
 
 
